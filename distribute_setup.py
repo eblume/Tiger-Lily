eca97dcd4c22c10710bb9e1ca051d1bfbf51d202
Erich Blume <blume.erich@gmail.com>
2011-09-14 10:08:18 -0700

    Added setup.py stuff - it seems to work well

---
+++ distribute_setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
--- setup.py
+++ setup.py
@@ -20,4 +20,40 @@
 #   along with Tiger Lily.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
+import glob
 
+from setuptools import setup, find_packages
+
+# The following is taken from python.org:
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name = 'tigerliy',
+    version = '0.1-dev',
+    packages = find_packages(),
+    scripts = glob.glob(os.path.join(os.path.dirname(__file__),'scripts/*')),
+    
+    # Required packages
+    install_requires = [],
+    
+    # Non-python files that are required by the install
+    package_data = {
+        # 'package_name' : ['*.txt','*.dat'],
+    },
+
+    author = 'Erich Blume',
+    author_email = 'blume.erich@gmail.com',
+    description = ('Biological science tools for Python 3, with an emphasis '
+                  'on speed for high throughput sequencing.'),
+    license = 'GPLv3',
+    keywords = 'python3 bioinformatics bio sequencing',
+    url = None,
+    long_description = read('README'),
+)
