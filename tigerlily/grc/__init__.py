5e203a8acb0a04b6f342b9a340efc0c928043fc9
Erich Blume <blume.erich@gmail.com>
2011-09-16 15:29:51 -0700

    Added init loaders

--- tigerlily/grc/__init__.py
+++ tigerlily/grc/__init__.py
@@ -28,3 +28,6 @@ other online resource entities such as NCBI, Sanger Institute, and the
 UCSC Genome Browser.
 """
 
+from tigerlily.grc.genome import (GRCGenome, ReferenceGenome, 
+    SUPPORTED_ASSEMBLIES, DEFAULT_ASSEMBLY)
+
