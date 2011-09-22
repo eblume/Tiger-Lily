9fc14e898b9fc1f2720493f5278447f5c027b1ba
Erich Blume <blume.erich@gmail.com>
2011-09-21 22:48:35 -0700

    Add utility package

---
+++ tigerlily/utility/__init__.py
@@ -0,0 +1,23 @@
+# __init__.py - Package meta file for tigerlily.utility
+# Authors:
+#   * Erich Blume <blume.erich@gmail.com>
+#
+# Copyright 2011 Erich Blume <blume.erich@gmail.com>
+#
+#   This file is part of Tiger Lily.
+#
+#   Tiger Lily is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Tiger Lily is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Tiger Lily.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
