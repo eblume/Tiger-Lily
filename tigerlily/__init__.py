39cd0ff9af291163eccee8722bf6f14ae53f91a2
Erich Blume <blume.erich@gmail.com>
2011-09-22 12:06:12 -0700

    Use versioning for the docs & package

--- tigerlily/__init__.py
+++ tigerlily/__init__.py
@@ -20,3 +20,19 @@
 #   along with Tiger Lily.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+# VERSION_INFO
+# A tuple of strings (version,release).
+#    version - A string of the form A.B where A is the 'Major Version' and
+#              B is the 'Minor Version'.
+#    release - The name of the release that this particular instance (or tag)
+#              of the package comes from. This will generally be of the form
+#              MAJOR.MINOR.RELEASE_NUMBER for final releases, with the following
+#              alternate forms for non-final releases:
+#
+#              MAJOR.MINOR.RELEASE_NUMBER-rc# : A Release Candidate, the #th in
+#                                               the series for this release.
+#               
+#              MAJOR.MINOR-dev : A development build. This is not actually
+#                                 a 'release' at all.
+
+VERSION_INFO = ('0.1','0.1-dev')
