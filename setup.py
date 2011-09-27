9395b3817d9c75547f02913fb56fc3ced269032f
Erich Blume <blume.erich@gmail.com>
2011-09-27 01:16:40 -0700

    Now can successfully run bootstrapping tests, yay

--- setup.py
+++ setup.py
@@ -69,4 +69,6 @@ setup(
     url = 'https://github.com/eblume/Tiger-Lily',
     download_url='https://github.com/eblume/Tiger-Lily/tarball/master',
     long_description = read('README.rst'),
+
+    test_suite = 'tests',
 )
