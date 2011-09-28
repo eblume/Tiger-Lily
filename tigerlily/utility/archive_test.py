d13ccb6366cefae3584d967393e76ca2562333db
Erich Blume <blume.erich@gmail.com>
2011-09-28 00:05:53 -0700

    Completed unit tests for archive.py

--- tigerlily/utility/archive_test.py
+++ tigerlily/utility/archive_test.py
@@ -32,6 +32,7 @@ import unittest
 import os
 
 import tigerlily.utility.archive as ar
+from tigerlily.sequences import FASTA
 
 
 class ConsoleDownloaderTests(unittest.TestCase):
@@ -41,29 +42,47 @@ class ConsoleDownloaderTests(unittest.TestCase):
     def setUp(self):
         """archive.py: Create the testing environment"""
         self.test_dir = os.path.join(os.dirname(__file__),'test_archives')
-        self.assertTrue(os.path.isdir(self.test_dir))
-
+        self.targz = os.path.join(self.test_dir, 'test_fasta_archive.tar.gz')
+        self.tarbz2 = os.path.join(self.test_dir, 'test_fasta_archive.tar.bz2')
+        self.tar = os.path.join(self.test_dir, 'test_fasta_archive.tar')
+        self.zip = os.path.join(self.test_dir, 'test_fasta_archive.zip')
 
     def test_targz(self):
         "archive.py: Test .tar.gz archive support"
-        pass
+        arch = ar.Archive(filepath=self.targz)
+        self._handle_arch(arch)
 
     def test_tarbz2(self):
         "archive.py: Test .tar.bz2 archive support"
-        pass
+        arch = ar.Archive(filepath=self.tarbz2)
+        self._handle_arch(arch)
 
     def test_tar(self):
         "archive.py: Test .tar archive support"
-        pass
+        arch = ar.Archive(filepath=self.tar)
+        self._handle_arch(arch)
 
     def test_zip(self):
         "archive.py: Test .zip archive support"
-        pass
+        arch = ar.Archive(filepath=self.zip)
+        self._handle_arch(arch)
 
-    def test_equivalence(self):
-        "archive.py: Test equivalent archives in different formats match"
-        pass
+    def _handle_arch(self,arch)
+        self.assertEqual(len(arch.getnames()),4)
 
+        fasta_files = [f for f in arch.getfasta()]
+        self.assertEqual(len(fasta_files),3)
+
+        nofasta_files = [f for f in arch.getnofasta()]
+        self.assertEqual(len(nofasta_files),1)
+
+        # Finally, just test to make sure that FASTA can handle the test files
+        # This isn't really a test of this unit, but it's a logical extension
+        # and it would be bad if it failed, so let's do the test.
+        for fasta_fileobj in arch.getfasta():
+            fasta = FASTA(fasta_fileobj)
+            self.assertTrue(len(fasta.sequence) > 0)
+            self.assertTrue(fasta.identifier.startswith('seq'))
     
         
         
